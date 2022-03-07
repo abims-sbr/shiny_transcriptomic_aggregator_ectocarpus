a644d39c2fe310150e0bc1076fd5af5489bc6aad
Romain Dallet <romain.dallet@sb-roscoff.fr>
2022-03-07 14:16:25 +0100

    Fix import files issue

--- private/import_server.R
+++ private/import_server.R
@@ -1,30 +1,59 @@
 # Get data from files
-# TPMS
-observeEvent(input$tpms_file, {
-	tpms_data_table(getDataFrameFromFile(input$tpms_file$datapath))
-})
-# Genes Data
-observeEvent(input$genes_data_file, {
-	genes_data_file<-getDataFrameFromFile(input$genes_data_file$datapath)
-	genes_data_table(merge_duplicated_data(genes_data_file))
-})
-# Samples Data
-observeEvent(input$samples_data_file, {
-	samples_data_file <- getDataFrameFromFile(input$samples_data_file$datapath)
-	if("private" %in% tolower(colnames(samples_data_file))){
-		# If public instance, took of private sample data
-		if(instance_tag == "public"){
-			samples_data_file <- samples_data_file[toupper(samples_data_file[,"private"]) == "FALSE",]
-		}
-		# Remove private column from the table
-		samples_data_file["private"] <- NULL
+observeEvent(input$import_files, {
+	# Genes
+	if(!is.null(input$genes_data_file)){
+		genes_data_table(getDataFrameFromFile(input$genes_data_file$datapath))	
 	}
-	samples_data_table(samples_data_file)   	
+	# Samples Data
+	if(!is.null(input$samples_data_file)){
+		samples_data_file <- getDataFrameFromFile(input$samples_data_file$datapath)
+		if("private" %in% tolower(colnames(samples_data_file))){
+			# If public instance, took of private sample data
+			if(instance_tag == "public"){
+				samples_data_file <- samples_data_file[toupper(samples_data_file[,"private"]) == "FALSE",]
+			}
+			# Remove private column from the table
+			samples_data_file["private"] <- NULL
+		}
+		samples_data_table(samples_data_file)		
+	}
+	# TPMS
+	if(!is.null(input$tpms_file)){
+		tpms_data_table(getDataFrameFromFile(input$tpms_file$datapath))	
+	}
 })
 
 # Reset Files
 observeEvent(input$reset_files, {
-	tpms_data_table(NULL)
-	genes_data_table(NULL)
-	samples_data_table(NULL)
+
+	if(!is.null(genes_data_input)){
+    	genes_data_table(getDataFrameFromFile(genes_data_input))
+    } else {
+    	genes_data_table(NULL)
+    }
+    reset('genes_data_file')
+
+    if(!is.null(samples_data_input)){
+		samples_data_file <- getDataFrameFromFile(samples_data_input)
+		if("private" %in% tolower(colnames(samples_data_file))){
+			# If public instance, took of private sample data
+			if(instance_tag == "public"){
+				samples_data_file <- samples_data_file[toupper(samples_data_file[,"private"]) == "FALSE",]
+			}
+			# Remove private column from the table
+			samples_data_file["private"] <- NULL
+		}
+		samples_data_table(samples_data_file)
+	} else {
+		samples_data_table(NULL)
+	}
+	reset('samples_data_file')
+
+	if(!is.null(tpms_input)){
+		tpms_data_table(getDataFrameFromFile(tpms_input))
+    } else {
+    	tpms_data_table(NULL)
+    }
+    reset('tpms_file')
+
 })
--- private/import_ui.R
+++ private/import_ui.R
@@ -6,15 +6,6 @@ tabItem(tabName = "import_tab",
 		width = 12,
 		column(12,
 			fluidRow(
-			 	fileInput(
-			   		inputId = "tpms_file", 
-			   		label = "Import TPMs File (.csv)",
-			        multiple = FALSE,
-			        accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),
-			        width = "100%"
-			    )
-			),
-			fluidRow(
 			   	fileInput(
 			   		inputId = "genes_data_file", 
 			   		label = "Import Genes Data File (.csv)",
@@ -33,6 +24,20 @@ tabItem(tabName = "import_tab",
 			    )
 		    ),
 			fluidRow(
+			 	fileInput(
+			   		inputId = "tpms_file", 
+			   		label = "Import TPMs File (.csv)",
+			        multiple = FALSE,
+			        accept = c("text/csv","text/comma-separated-values,text/plain",".csv"),
+			        width = "100%"
+			    )
+			),
+			fluidRow(
+				actionButton(
+					inputId = "import_files",
+					label = "Import",
+					class = "btn btn-primary"
+				),
 			    actionButton(
 			    	inputId = "reset_files",
 			    	label = "Reset"
